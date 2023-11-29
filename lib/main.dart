b58cb5c9ced80095e9427b85349f1ba6719d49f2
halfmoon-mind <5338095@gmail.com>
2023-11-29 10:15:32 +0900

    feat: daily formating

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 import 'package:issue_tracker_v2/detail_screen.dart';
 
-void main() {
+void main() async {
+  await initializeDateFormatting();
   runApp(const MyApp());
 }
 
@@ -16,6 +20,14 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      localizationsDelegates: const [
+        GlobalCupertinoLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('ko', 'KR'),
+      ],
       home: const IssueTrackerMain(),
     );
   }
@@ -46,82 +58,81 @@ class _IssueTrackerMainState extends State<IssueTrackerMain> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-            backgroundColor: Colors.white,
-            body: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 80),
-                  Row(
-                    children: [
-                      const Text(
-                        '2023.11.06.(금)',
-                        style: TextStyle(fontWeight: FontWeight.w600, fontSize: 32, color: Colors.black),
-                      ),
-                      const SizedBox(width: 14),
-                      Column(
-                        children: [
-                          Transform(
-                              alignment: Alignment.center,
-                              transform: Matrix4.rotationZ(90 * 3.1415926535 / 180),
-                              child: const Icon(Icons.arrow_back_ios_rounded)),
-                          Transform(
-                              alignment: Alignment.center,
-                              transform: Matrix4.rotationZ(270 * 3.1415926535 / 180),
-                              child: const Icon(Icons.arrow_back_ios_rounded)),
-                        ],
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 12,
-                  ),
-                  const Text(
-                    '11.2 ~ 11.6 트렌드',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 6,
-                  ),
-                  const SizedBox(
-                    width: 150,
-                    child: Divider(
-                      thickness: 2,
-                      color: Color(0xFFCCCCCC),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 26,
-                  ),
-                  Expanded(
-                    child: ListView.separated(
-                      itemCount: 10,
-                      itemBuilder: (context, index) {
-                        return RankingCell(index: index);
-                      },
-                      separatorBuilder: (BuildContext context, int index) {
-                        return const SizedBox(
-                          height: 10,
-                        );
-                      },
-                    ),
-                  ),
-                ],
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Text(
+                  DateFormat('yyyy.MM.dd. (E)', 'ko_KR').format(DateTime.now()),
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 26,
+                      color: Colors.black),
+                ),
+                const SizedBox(width: 14),
+                Column(
+                  children: [
+                    Transform(
+                        alignment: Alignment.center,
+                        transform: Matrix4.rotationZ(90 * 3.1415926535 / 180),
+                        child: const Icon(Icons.arrow_back_ios_rounded)),
+                    Transform(
+                        alignment: Alignment.center,
+                        transform: Matrix4.rotationZ(270 * 3.1415926535 / 180),
+                        child: const Icon(Icons.arrow_back_ios_rounded)),
+                  ],
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            const Text(
+              '11.2 ~ 11.6 트렌드',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w700,
               ),
-            )));
+            ),
+            const SizedBox(
+              height: 6,
+            ),
+            const SizedBox(
+              width: 150,
+              child: Divider(
+                thickness: 2,
+                color: Color(0xFFCCCCCC),
+              ),
+            ),
+            Expanded(
+              child: ListView.separated(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return RankingCell(index: index);
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  return const SizedBox(
+                    height: 10,
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
 class RankingCell extends StatefulWidget {
-  int index;
+  final int index;
 
-  RankingCell({
+  const RankingCell({
     super.key,
     required this.index,
   });
@@ -130,7 +141,8 @@ class RankingCell extends StatefulWidget {
   State<RankingCell> createState() => _RankingCellState();
 }
 
-class _RankingCellState extends State<RankingCell> with TickerProviderStateMixin {
+class _RankingCellState extends State<RankingCell>
+    with TickerProviderStateMixin {
   late AnimationController _animationController;
   late Animation<double> _animation;
 
@@ -140,11 +152,13 @@ class _RankingCellState extends State<RankingCell> with TickerProviderStateMixin
   void initState() {
     super.initState();
 
-    _animationController = AnimationController(vsync: this, duration: const Duration(milliseconds: 100));
-    _animation = Tween<double>(begin: 1, end: 0.96).animate(_animationController)
-      ..addListener(() {
-        setState(() {});
-      });
+    _animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 100));
+    _animation =
+        Tween<double>(begin: 1, end: 0.96).animate(_animationController)
+          ..addListener(() {
+            setState(() {});
+          });
   }
 
   @override
@@ -174,7 +188,8 @@ class _RankingCellState extends State<RankingCell> with TickerProviderStateMixin
       onTap: () async {
         print('onTap');
         await Future.delayed(const Duration(milliseconds: 30));
-        Navigator.of(context).push(MaterialPageRoute(builder: (context) => const DetailScreen()));
+        Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => const DetailScreen()));
       },
       child: ScaleTransition(
         scale: _animation,
@@ -190,17 +205,23 @@ class _RankingCellState extends State<RankingCell> with TickerProviderStateMixin
                 const SizedBox(width: 8),
                 SizedBox(
                     width: 40,
-                    child:
-                        Text('${widget.index + 1}', style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w800))),
+                    child: Text('${widget.index + 1}',
+                        style: const TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w800))),
                 const Row(
                   children: [
-                    Text('장인호', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600)),
+                    Text('장인호',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w600)),
                     SizedBox(
                       width: 10,
                     ),
                     Text(
                       'new',
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600, color: Colors.red),
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.red),
                     )
                   ],
                 ),
